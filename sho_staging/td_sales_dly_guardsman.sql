644d639a8eb6568e1ad1914ee8680f9500e7ce4e
jmarlo1 <james.marlow@shos.com>
2018-09-26 16:49:43 -0500

    Added division to guardsman staging table

--- sho_staging/td_sales_dly_guardsman.sql
+++ sho_staging/td_sales_dly_guardsman.sql
@@ -1,7 +1,12 @@
-create table teradata.td_sales_dly_guardsman (
-	TRS_DT date null,
-	LOC_NO int null,
-	NetSales float null,
-	UnitsSold int null,
-	REC_LOAD_DTM datetime default current_timestamp
-)
+CREATE TABLE [teradata].[td_sales_dly_guardsman](
+	[TRS_DT] [date] NULL,
+	[LOC_NO] [int] NULL,
+	[DIV_NO] [int] null,
+	[NetSales] [float] NULL,
+	[UnitsSold] [int] NULL,
+	[REC_LOAD_DTM] [datetime] NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [teradata].[td_sales_dly_guardsman] ADD  DEFAULT (getdate()) FOR [REC_LOAD_DTM]
+GO
